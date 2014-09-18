a528e1bfaea4f1cf2a71f7094861e4de0474bde5
rparige <raoparige@msn.com>
2014-09-17 21:32:40 -0700

    Committing working Assignment 2 Code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+Q## We have two functions for caching the inverse of a matrix
+## 1. makeCacheMatrix creates a list of functions to 
+##   set and get the input matrix, and set the inverse and get the inverse of the input matrix and get the matrix
+##
+## 2. cacheSolve function creates the inverse of the input matrix for the first run and return the cached inverse in all subsequent runs
+##   for a given matrix.  
 
-## Write a short comment describing this function
+##	This function also checks for non-square matrix input and return a message stating there is no inverse
+##	This function also works with a 1x1 matrix 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x=matrix()) {
+        m <- matrix()
+        set <- function(y) {
+                x <<- y
+                m <<- matrix()
+			
+	  }
+        get <- function() x
+	  setinverse <- function(solvem) m <<- solvem
+        getinverse <- function() m 
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {        
+	if ( nrow(x$get()) != ncol(x$get()) )
+	{
+		message("NOT A SQUARE MATRIX - THERE IS NO INVERSE!")
+		message("INPUT MATRIX IS: ")
+		return(x$get())
+	}
+	m <- x$getinverse()
+      
+	if(length(m) == length(x$get()) & !is.na(m[1,1])) 
+	{
+      	message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
